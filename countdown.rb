963fdc4c3fc309d0c743381f75d3552d85a12933
Patrick Lynch <pajlynch@gmail.com>
2021-03-31 23:34:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,13 @@ end
 
 countdown(10)
 
-def countdown_with_sleep
-
+def countdown_with_sleep(number)
+  while number > 0
+   sleep 5
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
